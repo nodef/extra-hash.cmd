77f39b99729d630563213aad0852004a8a9ea659
Subhajit Sahu <wolfram77@gmail.com>
2016-05-27 00:38:17 +0530

    program: getopt created to get options for omd5
    but nor i realize, args should support values option :expressionless:

--- cs-md5/Program.cs
+++ cs-md5/Program.cs
@@ -8,11 +8,18 @@ using Map = System.Collections.Generic.Dictionary<string, string>;
 namespace cs_md5 {
 	class Program {
 		static void Main(string[] args) {
-			Map opt = new Map() {[""]=""};
+			Map opt = GetOpt(args);
 			MD5 md5 = MD5.Create();
 			Stream stream = Console.OpenStandardInput();
-			String sout = BitConverter.ToString(md5.ComputeHash(stream)).Replace("-", "").ToLower();
+			string sout = BitConverter.ToString(md5.ComputeHash(stream)).Replace("-", "").ToLower();
 			Console.WriteLine(sout);
+		}
+
+		// get omd5 options
+		private static Map GetOpt(string[] args) {
+			Map name = new Map() {["--algo"]="-a"};
+			Regex opt = new Regex("-(a)", RegexOptions.IgnoreCase);
+			return ArgsMap(new Map(), args, name, opt);
 		}
 
 		// get command-line arguments as a map
