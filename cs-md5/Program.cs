fd34f2b7a9b24a958166b40a2b405882b330abc6
Subhajit Sahu <wolfram77@gmail.com>
2016-05-26 23:35:23 +0530

    program: get md5 checksum from stdin
    tested this using "cat <file> | cs-md5". :smile:

--- cs-md5/Program.cs
+++ cs-md5/Program.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// OMD5 - calculate md5 checksum for stdin or a file
+using System;
+using System.IO;
+using System.Security.Cryptography;
+
 
 namespace cs_md5 {
 	class Program {
 		static void Main(string[] args) {
+			MD5 md5 = MD5.Create();
+			Stream stream = Console.OpenStandardInput();
+			String sout = BitConverter.ToString(md5.ComputeHash(stream)).Replace("-", "").ToLower();
+			Console.WriteLine(sout);
 		}
 	}
 }
