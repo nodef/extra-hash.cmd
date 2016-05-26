8c25ae3c1bb69cda792fb25dba6de0f90d6a65b8
Subhajit Sahu <wolfram77@gmail.com>
2016-05-27 00:25:50 +0530

    program: argsmap created to read command-line arguments as map

--- cs-md5/Program.cs
+++ cs-md5/Program.cs
@@ -2,15 +2,27 @@
 using System;
 using System.IO;
 using System.Security.Cryptography;
-
+using System.Text.RegularExpressions;
+using Map = System.Collections.Generic.Dictionary<string, string>;
 
 namespace cs_md5 {
 	class Program {
 		static void Main(string[] args) {
+			Map opt = new Map() {[""]=""};
 			MD5 md5 = MD5.Create();
 			Stream stream = Console.OpenStandardInput();
 			String sout = BitConverter.ToString(md5.ComputeHash(stream)).Replace("-", "").ToLower();
 			Console.WriteLine(sout);
+		}
+
+		// get command-line arguments as a map
+		private static Map ArgsMap(Map m, string[] args, Map name, Regex opt) {
+			for(int i=0; i<args.Length; i++) {
+				string k = name.ContainsKey(k=args[i]) ? name[k] : k;
+				string v = opt.Match(k).Length == k.Length ? args[++i] : "";
+				m.Add(k, v);
+			}
+			return m;
 		}
 	}
 }
